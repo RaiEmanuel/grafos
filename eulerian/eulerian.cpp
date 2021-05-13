d440bcc485e306446a0e84e3aa788d1655bc2ef9
duke <rai_emanuel@hotmail.com>
2021-05-13 17:12:26 -0300

    correção do ciclo euleriano

--- eulerian/eulerian.cpp
+++ eulerian/eulerian.cpp
@@ -91,6 +91,8 @@ int main(int argc, char const *argv[])
     vector<Node *> nodesNotCircuit;
     set<Edge *> edgesCircuit;
     set<Edge *> edgesNotCircuit;
+    vector<Node *> path, subPath;
+
     unsigned short int nNodes, nEdges;
     file >> nNodes;
 
@@ -122,30 +124,94 @@ int main(int argc, char const *argv[])
         {
             Edge *e = new Edge(pNodeA, pNodeB, weight); //deletar no fim
             edgesNotCircuit.insert(e);
-            //cout << "++++++++++++++++++++ " << lookupNodeinEdge(getPointerNode('C',nodesCircuit, nodesNotCircuit), e) << endl;
-            cout << "Aresta criada com sucesso " << *e << endl;
+           // cout << "Aresta criada com sucesso " << *e << endl;
         }
-        cout << "-------------------------------" << endl;
     }
     //comeca a analise
     //====================================================
-    Node *initNode;
-    bool isCompletedCicle = false;
+    Node *initNode;//ponteiro do no que comeca o ciclo
+    bool isCompletedCicle = false;//true para dizer que completou um ciclo
+    bool initCicle = false;//true para saber se comecou um novo ciclo
+
     for (auto pNodeInit = nodesNotCircuit.begin(); pNodeInit != nodesNotCircuit.end();)
     {
         initNode = *pNodeInit;
-        //cout << "procurando mais um ciclo a partir de " << *initNode << endl;
-        //initNode = *pNodeInit; //tenta proximo no do circuito a achar ligacao com uma aresta ainda nao visitada
+        //ve se no comeca um ciclo
         Node *adjacentNode = initNode; //no adjacent para iterar formando circuito. mesmo que no w do circuito
         //procura uma aresta com um no que nao esteja na lista dos que estao no circuito
         for (auto pEdgeCurrent = edgesNotCircuit.begin(); pEdgeCurrent != edgesNotCircuit.end();)
         {
-           // cout << "no avaliado no momento " << *adjacentNode << endl;
-            Edge *searchedEdge = lookupNodeinEdge(adjacentNode, *pEdgeCurrent); //verifica se o no inicial esta em alguma aresta nao pertencente ao circuito
-            //cout << "achou aresta? " << searchedEdge << endl;
+            Edge *searchedEdge = lookupNodeinEdge(adjacentNode, *pEdgeCurrent); //verifica se o no inicial esta em alguma aresta nao pertencente ao circuito   
             if (searchedEdge != nullptr)
             {
-               // cout << "find " << *searchedEdge << endl;
+                initCicle = true;
+                path.push_back(adjacentNode);
+                edgesCircuit.insert(searchedEdge); //inserre a aresta no circuito
+                nodesCircuit.insert(adjacentNode); //adiciona o no nos ja pertencentes ao circuito
+                if (searchedEdge->fromNode != adjacentNode)
+                {
+                    adjacentNode = searchedEdge->fromNode; //recebe outra extremidade da aresta. esse no vai procurar uma aresta nova para anexar
+                }
+                else if (searchedEdge->targetNode != adjacentNode)
+                {
+                    adjacentNode = searchedEdge->targetNode; //recebe outra extremidade da aresta. esse no vai procurar uma aresta nova para anexar
+                }
+                edgesNotCircuit.erase(searchedEdge);    //deleta aresta das que estao fora do circuito
+                auto it = find(nodesNotCircuit.begin(), nodesNotCircuit.end(), adjacentNode);
+                if(it != nodesNotCircuit.end()) nodesNotCircuit.erase(it);//apaga no adjacente dos nao pertencentes ao circuito
+                pEdgeCurrent = edgesNotCircuit.begin(); //volta para a primeira aresta
+                isCompletedCicle = false;               //se achou aresta entao teve mais uma execucao em um ciclo
+                //verifica se proximo no e o inicial
+                if (initNode == adjacentNode)
+                {
+                    cout << "==================== Fechou primeiro ciclo ==========================" << endl;
+                    pNodeInit = nodesNotCircuit.end() - 1; 
+                    pEdgeCurrent = edgesNotCircuit.end();//para sair do ciclo
+                    isCompletedCicle = true;
+                    path.push_back(initNode);
+                    for(auto k = path.begin(); k != path.end(); ++k){
+                        cout << (*k)->id << " -> ";
+                    }cout << endl;
+                    initCicle = false;
+                }
+            }else ++pEdgeCurrent; //avalia proxima aresta
+        }//acabou todas arestas para o no x
+        ++pNodeInit;
+        if(initCicle == true){
+            //iniciou o ciclo e travou
+            cout << "acabaram possibilidades e não há para onde ir"<<endl;
+            cout << "+------------------+" << endl;
+            cout << "| Nao eh euleriano |" << endl;
+            cout << "+------------------+" << endl;
+            return EXIT_SUCCESS;
+        }
+        /*if(isCompletedCicle == true){
+            pNodeInit = nodesNotCircuit.end();
+        }*/
+        /*if (edgesNotCircuit.size() == 0)
+        {
+            pNodeInit = nodesNotCircuit.end(); //para fechar o for dos nos do circuito
+        }*/
+        //zerou arestas nao analisadas
+    }
+    cout << "comecando segunda analise" << endl;
+    //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    //reinicia processo
+    //iniciando novamente, mas agora com um vertice do circuito
+    //precisa ter arestas nao adicionadas no ciruito para poder avaliar
+    for (auto pNodeInit = nodesCircuit.begin(); pNodeInit != nodesCircuit.end();)
+    {
+        initNode = *pNodeInit;
+        //ve se no comeca um ciclo
+        Node *adjacentNode = initNode; //no adjacent para iterar formando circuito. mesmo que no w do circuito
+        //procura uma aresta com um no que nao esteja na lista dos que estao no circuito
+        for (auto pEdgeCurrent = edgesNotCircuit.begin(); pEdgeCurrent != edgesNotCircuit.end();)
+        {
+            Edge *searchedEdge = lookupNodeinEdge(adjacentNode, *pEdgeCurrent); //verifica se o no inicial esta em alguma aresta nao pertencente ao circuito   
+            if (searchedEdge != nullptr)
+            {
+                initCicle = true;
+                subPath.push_back(adjacentNode);
                 edgesCircuit.insert(searchedEdge); //inserre a aresta no circuito
                 nodesCircuit.insert(adjacentNode); //adiciona o no nos ja pertencentes ao circuito
                 if (searchedEdge->fromNode != adjacentNode)
@@ -162,106 +228,39 @@ int main(int argc, char const *argv[])
                 //verifica se proximo no e o inicial
                 if (initNode == adjacentNode)
                 {
-                    cout << "==================== Fechou mais um ciclo ==========================" << endl;
-                    pNodeInit = nodesNotCircuit.begin(); //comeca novamente a analsiar do primeiro no do circuito
+                    cout << "==================== Fechou ciclo intermediario ==========================" << endl;
+                    pNodeInit = nodesCircuit.begin(); //comeca novamente a analsiar do primeiro no do circuito
+                    //pEdgeCurrent = edgesNotCircuit.begin(); redundacia
                     isCompletedCicle = true;
-                    for(auto p = edgesCircuit.begin(); p != edgesCircuit.end(); ++p){
-                        cout << **p << endl;
-                    }
+                    initCicle = false;
+                    auto nodeW = find(path.begin(), path.end(), initNode);
+                    path.insert(nodeW, subPath.begin(), subPath.end());
+                    subPath.clear();
+                    for(auto k = path.begin(); k != path.end(); ++k){
+                        cout << (*k)->id << "-> ";
+                    }cout << endl;
                 }
             }
             else
                 ++pEdgeCurrent; //avalia proxima aresta
         }
         //acabou todas arestas para o no x
-        //cout << "Nao achou nenhum aresta nao visitada com " << *adjacentNode << ", vamos tentar o proximo no" << endl
-             //<< endl;
         ++pNodeInit;
+        //iniciou o ciclo e travou
+        if(initCicle == true){
+            cout << "acabaram possibilidades e não há para onde ir"<<endl;
+            cout << "+------------------+" << endl;
+            cout << "| Nao eh euleriano |" << endl;
+            cout << "+------------------+" << endl;
+            return EXIT_SUCCESS;
+        }
         //zerou arestas nao analisadas
         if (edgesNotCircuit.size() == 0)
         {
             //cout << "Nao ha mais arestas para analisar" << endl;
-            pNodeInit = nodesNotCircuit.end(); //para fechar o for dos nos do circuito
-        }
-    }
-
-    //-----------------------------------------------------------------------
-    //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    //reinicia processo
-    //iniciando novamente, mas agora com um vertice do circuito
-
-    //isCompletedCicle = false;
-    //precisa ter arestas nao adicionadas no ciruito para poder avaliar
-    for (auto pNodeInit = nodesCircuit.begin(); pNodeInit != nodesCircuit.end() && edgesNotCircuit.size() != 0;)
-    {
-        initNode = *pNodeInit;
-        //cout << "procurando mais um ciclo a partir de " << *initNode << endl;
-        //initNode = *pNodeInit; //tenta proximo no do circuito a achar ligacao com uma aresta ainda nao visitada
-        Node *adjacentNode = initNode; //no adjacent para iterar formando circuito. mesmo que no w do circuito
-        //procura uma aresta com um no que nao esteja na lista dos que estao no circuito
-        for (auto pEdgeCurrent = edgesNotCircuit.begin(); pEdgeCurrent != edgesNotCircuit.end();)
-        {
-            //cout << "no avaliado no momento " << *adjacentNode << endl;
-            Edge *searchedEdge = lookupNodeinEdge(adjacentNode, *pEdgeCurrent); //verifica se o no inicial esta em alguma aresta nao pertencente ao circuito
-            //cout << "achou aresta? " << searchedEdge << endl;
-            if (searchedEdge != nullptr)
-            {
-                //cout << "find " << *searchedEdge << endl;
-                edgesCircuit.insert(searchedEdge); //inserre a aresta no circuito
-                nodesCircuit.insert(adjacentNode); //adiciona o no nos ja pertencentes ao circuito
-                if (searchedEdge->fromNode != adjacentNode)
-                {
-                    adjacentNode = searchedEdge->fromNode; //recebe outra extremidade da aresta. esse no vai procurar uma aresta nova para anexar
-                }
-                else if (searchedEdge->targetNode != adjacentNode)
-                {
-                    adjacentNode = searchedEdge->targetNode; //recebe outra extremidade da aresta. esse no vai procurar uma aresta nova para anexar
-                }
-                edgesNotCircuit.erase(searchedEdge);    //deleta aresta das que estao fora do circuito
-                pEdgeCurrent = edgesNotCircuit.begin(); //volta para a primeira aresta
-                isCompletedCicle = false;               //se achou aresta entao teve mais uma execucao em um ciclo
-                //verifica se proximo no e o inicial
-                if (initNode == adjacentNode)
-                {
-                    cout << "==================== Fechou mais um ciclo ==========================" << endl;
-                    pNodeInit = nodesCircuit.begin(); //comeca novamente a analsiar do primeiro no do circuito
-                    isCompletedCicle = true;
-                    for(auto p = edgesCircuit.begin(); p != edgesCircuit.end(); ++p){
-                        cout << **p << endl;
-                    }
-                }
-            }
-            else
-                ++pEdgeCurrent; //avalia proxima aresta
-        }
-        //acabou todas arestas para o no x
-        //cout << "Nao achou nenhum aresta nao visitada com " << *adjacentNode << ", vamos tentar o proximo no" << endl
-            // << endl;
-        ++pNodeInit;
-        //zerou arestas nao analisadas
-        if (edgesNotCircuit.size() == 0)
-        {
-           // cout << "Nao ha mais arestas para analisar" << endl;
             pNodeInit = nodesCircuit.end(); //para fechar o for dos nos do circuito
         }
     }
-
-    //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-/* 
-        6 10
-        A B 1
-        A C 1
-        B C 1
-        B D 1
-        C E 1
-        C D 1
-        B E 1
-        D F 1
-        E F 1
-        D E 1
-        */
-
 
     if (isCompletedCicle == true)
     {
@@ -275,6 +274,5 @@ int main(int argc, char const *argv[])
         cout << "| Nao eh euleriano |" << endl;
         cout << "+------------------+" << endl;
     }
-
     return EXIT_SUCCESS;
 }
